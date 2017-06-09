9bcc8a212647c90b6fdb4e2cead3ff10f4b92454
fakebenjay <fakebenjay@gmail.com>
2017-06-09 02:36:52 -0400

    Done.

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,17 @@
-# Implement your procedural solution here!
+def smallest_multiple(input)
+  array = (1..input).to_a
+  counter = 1
+  index = 0
+
+  ## array = [1, 2, 3, 4]
+
+  until index == array.length
+    if (input*counter) % array[index] == 0
+      index += 1
+    elsif (input*counter) % array[index] != 0
+      counter += 1
+      index = 0
+    end
+  end
+  return input*counter
+end
